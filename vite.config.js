5c9fcadd054967edf4603964dcd1d6e1c177fb1c
Berlin <3q.berlin@gmail.com>
2023-11-07 00:14:17 +0800

    first

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vite1101/' : '/',
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    ViteEjsPlugin(),
+  ],
+});
